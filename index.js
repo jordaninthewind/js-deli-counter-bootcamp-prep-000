999e1aad5b6209625f5d044c330a9949535fce85
jordaninthewind <jordan.kline@gmail.com>
2017-09-15 13:43:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function takeANumber(currentList, newPerson) {
 
 function nowServing(currentList) {
     // Returns first person in line
-  if (currentList.length > 0)
+  if (currentList.length > 0) {}
     return `Currently serving ${currentList[0]}.`;
     // Removes same individual from line.
     currentList.shift();
