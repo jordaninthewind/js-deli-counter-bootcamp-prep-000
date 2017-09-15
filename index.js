6ded88cb831ec89cfcb5e5b603a72b6693ca21d0
jordaninthewind <jordan.kline@gmail.com>
2017-09-15 14:07:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,13 +26,12 @@ function currentLine(line) {
   if(line.length === 0) {
     return 'The line is currently empty.';
   } else {
-    while(line.length > 0) {
-      var counter = line.length;
-      var returnArray = [counter + line[counter]];
-      counter--;
+    for(var i = 0; i < line.length; i++) {
+      var newArray = [line[i]];
+      
     }
     // Returns current line
-    return 'The line is currently: ' + ;
+    return 'The line is currently: ' + newArray;
   }
   // String = 'The line is current: ${[number + 1], ${number + 1}}'
 }
