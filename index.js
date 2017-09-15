c1478eb786647ab83c17295dbfd508a7874f909b
jordaninthewind <jordan.kline@gmail.com>
2017-09-15 13:33:06 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+  // Accept current line of people with name as parameter
+function takeANumber(list) {
+  // Function returns position in line
+
+  // Ensure position is not index
+}
+
+function nowServing() {
+    // Returns first person in line
+
+    // Removes same individual from line.
+
+    // IF line is empty, returns 'There is nobody waiting to be served!'
+}
+
+function currentLine() {
+  // Returns current line
+
+  // String = 'The line is current: ${[number + 1], ${number + 1}}'
+
+  // If no one is in line, returns 'The line is currently empty.'
+}
